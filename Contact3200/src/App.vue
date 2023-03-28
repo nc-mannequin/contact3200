3c6dad159bab19f02642ffd9267d906836e66af6
Mannequinn <napat.che@student.mahidol.edu>
2023-03-28 22:44:57 +0700

    Updated to be NapatBranch by Napat

--- Contact3200/src/App.vue
+++ Contact3200/src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
-import {  RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue'
-// import HelloWorld from './components/HelloWorld.vue'
-// import LoginPageView from './views/LoginPageView.vue';
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <NavBar/>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
   </header>
-  <RouterView/>
-  <!-- <RouterView v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component"/>
-    </transition>
-  </RouterView> -->
-  
 
-
+  <RouterView />
 </template>
 
 <style scoped>
